a9b85cbedffde26997d21ac2f68dc4e120a26a76
GrieferPig <grieferpig@163.com>
2023-01-13 22:26:49 +0800

    feat: add compresion on frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 // Plugins
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import viteCompression from 'vite-plugin-compression'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -9,13 +10,14 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
     }),
+    viteCompression(),
   ],
   define: { 'process.env': {} },
   resolve: {
